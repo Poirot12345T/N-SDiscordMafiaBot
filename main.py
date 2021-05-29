63196ea6aedbc48bc66c33aabe1f9da64cb1acc7
Poirot12345 T <82270962+Poirot12345T@users.noreply.github.com>
2021-05-29 19:46:39 +0200

    first basic commands

--- main.py
+++ main.py
@@ -2,11 +2,27 @@ import asyncio
 import discord
 from decouple import config
 
+
+prefix = config("PREFIX")
 client = discord.Client()
+info = "nothing special, I'm just a bot :person_shrugging:"
 
 @client.event
 async def on_ready():
     print("bot connected")
+
+@client.event
+async def on_message(message):
+    global prefix
+    if message.content.startswith(prefix):
+        if message.content.startswith(prefix + "ping"):
+            await message.channel.send("pong")
+            print('pinged using ping')
+        elif message.content.startswith(prefix + "info"):
+            await message.channel.send(info)
+        else:
+            await message.channel.send("unknown command")
+        
 
 
 client.run(config("TOKEN"))
