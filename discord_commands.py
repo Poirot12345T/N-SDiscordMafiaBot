6a9a7dce16259e4dfd632cbb932763a116cca6b0
Ivan Grigoriev <grigoriev.vanja@gmail.com>
2021-06-22 01:47:15 +0200

    Worked on embed.

--- discord_commands.py
+++ discord_commands.py
@@ -1,7 +1,8 @@
 import discord
 from discord.ext import commands
 import datetime
-import re
+import dateutil.parser
+import string
 
 
 class Commands(commands.Cog):
@@ -13,11 +14,21 @@ class Commands(commands.Cog):
         await ctx.send(f'Pong! {self.bot.latency * 1000:.0f}')
 
     @commands.command('poll')
-    async def poll(self, ctx: commands.Context, title: str, end_time: str):
+    async def poll(self, ctx: commands.Context, title: str, question: str, end_time: str = None, *args):
+        letters = (f':regional_indicator_{x}:' for x in string.ascii_lowercase)
 
-        embed = discord.Embed(title=title)
+        embed = discord.Embed(
+            title=title,
+            description=f'{question}\n'
+                        f'{"═" * 25}\n'
+                        f'{chr(10).join((f"{emoji} {answer}"for answer, emoji in zip(args, letters)))}\n'
+                        f'{"═" * 25}\n'
+                        f'Vote on the poll by reacting, or add\n'
+                        f'another option with `!add your-option-here`'
+        )
+
         await ctx.send(embed=embed)
 
 
-def setup(bot: commands.Context):
+def setup(bot: commands.Bot):
     bot.add_cog(Commands(bot))
